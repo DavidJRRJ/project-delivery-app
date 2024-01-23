0229b8224724e79dc6c32eb6270b91c79bfc463f
David Junior <david.lourenco94@gmail.com>
2024-01-23 17:44:19 -0300

    error jwt

--- back-end/src/utils/createToken.js
+++ back-end/src/utils/createToken.js
@@ -1,11 +1,13 @@
-require('dotenv');
-const jwt = require('jsonwebtoken');
-const jwtKey = require('fs').readFileSync('../back-end/jwt.evaluation.key', { encoding: 'utf-8' });
+require("dotenv");
+const jwt = require("jsonwebtoken");
+const jwtKey = require("fs").readFileSync("../../jwt.evaluation.key", {
+  encoding: "utf-8",
+});
 
 const createToken = (data) => {
   const token = jwt.sign({ data }, jwtKey, {
-    expiresIn: '15d',
-    algorithm: 'HS256',
+    expiresIn: "15d",
+    algorithm: "HS256",
   });
 
   return token;
